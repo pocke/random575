b16a32edff0898c3f983c222e7068cee24c32d05
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2020-03-01 23:23:37 +0900

    Split poems

--- lib/app.rb
+++ lib/app.rb
@@ -1,24 +1,5 @@
 require 'ovto'
-
-module Random575
-  class MainApp < Ovto::App
-    class State < Ovto::State
-    end
-
-    class Actions < Ovto::Actions
-      def dispatch(state:)
-        return state
-      end
-    end
-
-    class MainComponent < Ovto::Component
-      def render
-        o 'div' do
-          o 'text', 'hello, ovto'
-        end
-      end
-    end
-  end
-end
+require 'random575/main_app'
+require 'random575/source'
 
 Random575::MainApp.run(id: 'ovto')
---
+++ lib/random575/main_app.rb
@@ -0,0 +1,20 @@
+module Random575
+  class MainApp < Ovto::App
+    class State < Ovto::State
+    end
+
+    class Actions < Ovto::Actions
+      def dispatch(state:)
+        return state
+      end
+    end
+
+    class MainComponent < Ovto::Component
+      def render
+        o 'div' do
+          o 'text', 'hello, ovto'
+        end
+      end
+    end
+  end
+end
